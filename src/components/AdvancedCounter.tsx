2fe8158ed8ce319c6e30894abc44a629737f5ba9
uries242 <teykoa@gmail.com>
2026-05-16 12:32:38 -0400

    Added state for history tracking using useState hook, created variable to hold an array of past counts

--- src/components/AdvancedCounter.tsx
+++ src/components/AdvancedCounter.tsx
@@ -1,29 +1,39 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect } from "react";
 
 function AdvancedCounter() {
-  const [count, setCount] = useState(0)
-  const [step, setstep] = useState(1)
+  const [count, setCount] = useState(0);
+  const [step, setstep] = useState(1);
+  const [history, setHistory] = useState<number[]>([]);
 
   useEffect(() => {
-    document.title = `Count: ${count}`
-  }, [count])
-
+    document.title = `Count: ${count}`;
+  }, [count]);
 
   return (
     <>
       <section id="center">
-
         <div>
           <h4>Counter</h4>
           <p>Current count: {count}</p>
         </div>
 
-        <button type='button' onClick={() => setCount((count) => count - step)}>Decrement</button>
-        <button type='button' onClick={() => setCount((count) => count + step)}>Increment</button>
-        <button type='button' onClick={() => setCount(0)}>Reset</button>
-        
-        <label>Step Value:
-        <input type="number" value={step} onChange={(event) => setstep(Number(event.target.value))} />
+        <button type="button" onClick={() => setCount((count) => count - step)}>
+          Decrement
+        </button>
+        <button type="button" onClick={() => setCount((count) => count + step)}>
+          Increment
+        </button>
+        <button type="button" onClick={() => setCount(0)}>
+          Reset
+        </button>
+
+        <label>
+          Step Value:
+          <input
+            type="number"
+            value={step}
+            onChange={(event) => setstep(Number(event.target.value))}
+          />
         </label>
         <p>Changes saved</p>
         <br />
@@ -31,9 +41,8 @@ function AdvancedCounter() {
       <p>Count History:</p>
       <section id="spacer"></section>
       {}
-
     </>
-  )
+  );
 }
 
-export default AdvancedCounter
+export default AdvancedCounter;
